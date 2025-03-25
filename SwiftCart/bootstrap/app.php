48812610819afee9885c37e09a958f73c65b031d
Batki Domonkos <domonkosbatki98@gmail.com>
2025-03-25 20:42:48 +0100

    middleware fix

--- SwiftCart/bootstrap/app.php
+++ SwiftCart/bootstrap/app.php
@@ -13,10 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(CheckStoreOwner::class);
-        $middleware->append(CheckMapOwner::class);
-    })
+    ->withMiddleware(function (Middleware $middleware) {})
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
